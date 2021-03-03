c66cd350ffe2eb8e19f8ee414dd6e2dfe906539e
w0jtaz <wojtaz@vp.pl>
2021-03-03 12:23:25 +0100

    LAB_1

---
+++ CW_1/main.py
@@ -0,0 +1,25 @@
+from math import *
+a = 10
+b = 5
+c = 2 + 3j
+
+dodawanie = a + b
+odejmowanie = a - b
+mnozenie = a * b
+dzielenie = a / b
+dzielenie_calkowite = a // b
+potegowanie = a ** b
+print(dodawanie, odejmowanie, mnozenie, dzielenie, dzielenie_calkowite, potegowanie)
+print(pow(8, 2))
+a *= c
+print(sqrt(9))
+print(pi)
+print(u'inżynieria systemów informatycznych')
+print('Wynik działania jest równy a = %(zm)d' % {'zm' : 12})
+napis = 'isi'
+grupa = 2
+print('specjalizacja %s, grupa %d' % (napis, grupa))
+a = 5
+b = 3
+z = 5 - 3
+print('Wynik dzialania %(z1)d-%(z2)d=%(z3)d' % {'z1': a, 'z2': b, 'z3': z})
