8345529c858fe220719f4ced7cafd1e669ec4e0c
w0jtaz <wojtaz@vp.pl>
2021-03-03 13:04:11 +0100

    praca z lekcji

--- CW_1/venv/Scripts/activate.bat
+++ CW_1/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\projects\python\lekcja1\venv
+set VIRTUAL_ENV=C:\Users\Administrator\Documents\GitHub\wizualizacja_danych\test\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
