fb0a2539c72dbb86b96a444bece3a154455c8a0c
w0jtaz <wojtaz@vp.pl>
2021-04-20 21:50:52 +0200

    numpy2

--- numpy2_zadania/main.py
+++ numpy2_zadania/main.py
@@ -3,7 +3,8 @@ import numpy as np
 #Zadanie 1
 a = np.array([2,3,4,5])
 b = np.array([12,1,-7,2])
-print(np.dot(a,b))
+c = a*b
+print(c)
 
 #Zadanie 2
 a = np.array([[2,1,3], [-1,2,4], [-8,4,5]])
@@ -14,3 +15,8 @@ print('minimalne wartosci dla wierszy',a.min(axis=1))
 
 print('minimalne wartosci dla kolumn',b.min(axis=0))
 print('minimalne wartosci dla wierszy',b.min(axis=1))
+
+#Zadanie 3
+a = np.array([2,3,4,5])
+b = np.array([12,1,-7,2])
+print(np.dot(a,b))
