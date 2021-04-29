886352233ea51609e17a4abc8ca99877fd5e7aa3
w0jtaz <wojtaz@vp.pl>
2021-04-29 17:14:19 +0200

    pandas

--- pandas2_lekcja/main.py
+++ pandas2_lekcja/main.py
@@ -46,4 +46,58 @@ df['MA'] = df.rolling(window=50).mean()
 df.plot()
 plt.show()
 
+#wykres linniowy z danych
+plt.plot([1,2,3,4])
+plt.ylabel('jakieś liczby')
+plt.show()
 
+#wykres wektory
+plt.plot([1,2,3,4], [1,4,8,16], 'ro-')
+plt.axis([0, 6, 0, 20])
+plt.show()
+
+#wykresy nachodzące na siebie punkty
+plt.plot([1,2,3,4], [1,4,8,16], 'r')
+plt.plot([1,2,3,4], [1,4,8,16], 'o')
+plt.axis([0, 6, 0, 20])
+plt.show()
+
+#kilka wykresów w jednym
+t = np.arange(0., 5., 0.2)
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
+plt.show()
+
+x = np.linspace(0, 2, 100)
+plt.plot(x, x, label='postać linniowa')
+plt.plot(x, x**2, label='postać kwadratowa')
+plt.plot(x, x**3, label='postać sześcienna')
+plt.xlabel('etykieta x')
+plt.ylabel('etykieta y')
+plt.title('Prosty wykres')
+plt.legend()
+plt.show()
+
+#wykres sinus
+x = np.arange(0, 10, 0.1)
+s = np.sin(x)
+plt.plot(x, s, label='sin(x)')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.title('Wykres sin(x)')
+plt.legend()
+plt.show()
+
+#
+x1 = np.arange(0.0, 2.0, 0.02)
+x2 = np.arange(0.0, 2.0, 0.02)
+y1 = np.sin(2 * np.pi * x1)
+y2= np.cos(2 * np.pi * x1)
+plt.subplot(2,1,1)
+plt.plot(x1, y1, '-')
+plt.title('Dwa podwykresy')
+plt.ylabel('sin(x)')
+plt.subplot(2,1,2)
+plt.plot(x2, y2, 'r-')
+plt.xlabel('x')
+plt.ylabel('cos(x)')
+plt.show()
