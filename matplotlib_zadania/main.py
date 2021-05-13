55d15923595cc50fb9717efb1b9ba061a9a8e3b5
w0jtaz <wojtaz@vp.pl>
2021-05-13 14:58:46 +0200

    matplotlib update

--- matplotlib_zadania/main.py
+++ matplotlib_zadania/main.py
@@ -54,17 +54,19 @@ import matplotlib.pyplot as plt
 # plt.show()
 
 # #Zadanie 5
-df = pd.read_csv("iris.data", names=["sepal len", "sepal wid", "petal len", "petal wid", "class"])
-df = pd.DataFrame(df)
-df = df[["sepal len", "sepal wid", "class"]]
-plt.xlabel("sepal length")
-plt.ylabel("sepal width")
-data = {"a": df["sepal len"], "b": df["sepal wid"], "c": np.random.randint(0, 150, 150)}
-data["d"] = np.abs(data["a"]-data["b"])
-plt.scatter("a", "b", c="c", s="d", data=data)
-plt.title("Iris sepal length and width")
-plt.show()
+# df = pd.read_csv("iris.data", names=["sepal len", "sepal wid", "petal len", "petal wid", "class"])
+# df = pd.DataFrame(df)
+# df = df[["sepal len", "sepal wid", "class"]]
+# plt.xlabel("sepal length")
+# plt.ylabel("sepal width")
+# data = {"a": df["sepal len"], "b": df["sepal wid"], "c": np.random.randint(0, 150, 150)}
+# data["d"] = np.abs(data["a"]-data["b"])
+# plt.scatter("a", "b", c="c", s="d", data=data)
+# plt.title("Iris sepal length and width")
+# plt.show()
 
 #Zadanie 6
 # data = pd.ExcelFile("imiona.xlsx")
 # df = pd.read_excel(data, header=0)
+
+#Zadanie 7
